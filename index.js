2b33450a47e2528883702fb17d8eb81aa82ec21a
Abhishek <Abhishekkange@gmail.com>
2023-11-19 18:51:36 +0530

    bot sends and recives message

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const telegramBot = 'node-telegram-bot-api';
+const telegramBot = require('node-telegram-bot-api');
 const API = '6303358546:AAF76kR69yBbtpvJKc52ku-no0NhRtmm52o';
-const express = require('express');
-const app = express();
+
+const bot = new telegramBot(API,{polling:true});
+
+bot.on('message',(message)=>{
+
+    // Send a response message to the same chat where the message was received from
+    const chatId = message.chat.id;
+    const responseMessage = 'Hello '+message.text; // Replace with your response message
+
+    bot.sendMessage(chatId, responseMessage)
+        .then(sentMessage => {
+            console.log('Response message sent:', sentMessage.text);
+        })
+        .catch(error => {
+            console.error('Error sending response message:', error);
+        });
+    
+});
 
 
 
 
 
-app.listen(3000,()=>{
 
-    console.log('App is running at PORT 3000');
-})
+
