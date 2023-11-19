7c1ad5b2ad381c1f25bef4474439af71ea4611bd
Abhishek <Abhishekkange@gmail.com>
2023-11-19 18:09:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const telegramBot = 'node-telegram-bot-api';
+const API = '6303358546:AAF76kR69yBbtpvJKc52ku-no0NhRtmm52o';
+const express = require('express');
+const app = express();
+
+
+
+
+
+app.listen(3000,()=>{
+
+    console.log('App is running at PORT 3000');
+})
