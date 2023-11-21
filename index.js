8562f416889c96c5504ccac40a9edbc9e309aafc
Abhishek <Abhishekkange@gmail.com>
2023-11-21 12:52:19 +0530

    final commit

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 const telegramBot = require('node-telegram-bot-api');
-const dotenv = require('dotenv');
-const API = dotenv.SECRET_KEY;
+
+const API = "6303358546:AAF76kR69yBbtpvJKc52ku-no0NhRtmm52o"
 
 const bot = new telegramBot(API,{polling:true});
 
-bot.on('message',(message)=>{
+bot.onText(/\/start/, (msg) => {
 
-    // Send a response message to the same chat where the message was received from
-    const chatId = message.chat.id;
-    const responseMessage = 'Hello '+message.text; // Replace with your response message
+    bot.sendMessage(msg.chat.id, "Welcome", {
+    "reply_markup": {
+        "keyboard": [["shahrukh khan", "Second sample"],   ["Keyboard"], ["I'm robot"]]
+        }
+    });
+    
+    });
 
-    bot.sendMessage(chatId, responseMessage)
-        .then(sentMessage => {
-            console.log('Response message sent:', sentMessage.text);
-        })
-        .catch(error => {
-            console.error('Error sending response message:', error);
+    bot.on('message', (msg) => {
+
+        var Hi = "hi";
+        if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+          bot.sendMessage(msg.chat.id,"<b>bold</b> \n <i>italic</i> \n <em>italic with em</em> \n <a href=\"http://www.example.com/\">inline URL</a> \n <code>inline fixed-width code</code> \n <pre>pre-formatted fixed-width code block</pre>" ,{parse_mode : "HTML"});
+          }
+       });
+
+    bot.on('message', (message) => {
+        if (message.text === "shahrukh khan") {
+            bot.sendPhoto(
+                message.chat.id,
+                "https://filmfare.wwmindia.com/content/2019/oct/shahrukhkhan7676761570181830.jpg",
+                { caption: "This is Shahrukh Khan" }
+            );
+        }
+    });
+
+    bot.on('message', (msg) => {
+        var location = "location";
+        if (msg.text.indexOf(location) === 0) {
+            bot.sendLocation(msg.chat.id,44.97108, -104.27719);
+            bot.sendMessage(msg.chat.id, "Here is the point");
+    
+        }
+    });
+    
+    bot.on('message', (msg) => {
+
+        var what = "idiot";
+        if (msg.text.includes(what)) {
+        bot.kickChatMember(msg.chat.id,  msg.from.id);
+        }
+        
         });
     
-});
+
+
 
 
 
